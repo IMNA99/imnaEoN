c8f67fde11afb6d34a3669605de0e1ea0aeb4710
IMNA99 <100994640+IMNA99@users.noreply.github.com>
2022-03-21 13:47:48 +0900

    Add files via upload

--- EoN_3.c
+++ EoN_3.c
@@ -8,13 +8,12 @@ int main(void)
 	int iCount;
 
 	printf("6개의 수를 입력하시오. : ");
-	scanf("%d%d%d%d%d%d", &iValue[0], &iValue[1], &iValue[2], &iValue[3], &iValue[4], &iValue[5]);
+	scanf("%d, %d, %d, %d, %d, %d", &iValue[0], &iValue[1], &iValue[2], &iValue[3], &iValue[4], &iValue[5]);
 
 
 	for (iCount = 0; iCount < 6; iCount++)
 	{
 		iSum = iSum + iValue[iCount];
-
 	}
 
 	printf("6개의 수의 합은 %d입니다.\n", iSum);
