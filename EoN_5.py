b563d2f2ab4cf4cbf5086beef0a5ee8baabf2645
IMNA99 <100994640+IMNA99@users.noreply.github.com>
2022-04-04 02:02:05 +0900

    Create EoN_5.py

---
+++ EoN_5.py
@@ -0,0 +1,24 @@
+#수 찾기
+#입력 -> 배열 자르기 -> 오름차순 정렬 -> k번째 숫자 출력
+
+
+	#array 입력
+array = list(map(int, input(). split()))
+print(array)
+	#commands 입력
+commands = [list(map(int, input().split())) for _ in range(3)]
+print(commands)
+
+
+	#solution 함수
+def solution(array, commands) :
+	answer = []
+	for (i, j, k) in commands : #커멘드 순차 입력
+		arr = array[i-1:j] #i번째에서 j번째까지 자르기; i번째수는 array[i-1]
+		arr.sort() #오름차순 정렬
+		answer.append(arr[k-1]) #k번째 숫자 찾기
+		
+	return answer
+	
+	#출력
+print(solution(array, commands))
